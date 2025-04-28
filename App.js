8581e3adfcc897e73569dd75518b6c6766a8839f
Android Studio <devandroid@ort>
2025-04-28 11:58:38 -0300

    Implemento SplashScreen

--- App.js
+++ App.js
@@ -1,10 +1,52 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import './App.css';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+      const esperarAlgunosMilisegundos = async () => {
+      // Evitar que el splash screen se oculte automáticamente
+      await SplashScreen.preventAutoHideAsync();
+      try {
+          // Acá hacemos todas las llamadas asincrónicas que necesitamos.
+          // Por ejemplo precarga de Fonts, llamadas a APIs externas, etc...
+          
+          // Simular una demora de 5 segundos.
+          await new Promise(resolve => setTimeout(resolve, 5000));
+      } catch (e) {
+          console.warn(e);
+      } finally {
+          // Cambiar el estado de carga a falso y ocultar el splash screen
+          setIsLoading(false);
+          await SplashScreen.hideAsync();
+      }
+    };
+
+    esperarAlgunosMilisegundos();
+  }, []);
+
+  // Renderizado condicional:
+  // Mostrar una pantalla en blanco mientras se carga
+  if (isLoading) {
+    return null;
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to my App ♥</Text>
+      <View style={styles.buttonWrapper}>
+        <Button
+          title="Learn More"
+          color="#7fffd4" 
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +58,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  
+
+  buttonWrapper: {
+    padding: 10, 
+    borderRadius: 5, 
   },
 });
